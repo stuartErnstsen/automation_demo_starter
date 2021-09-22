55359e869b8baf1ed242677b4dd71cc25c81a006
stuartErnstsen <stuarternstsen@gmail.com>
2021-09-21 22:49:50 -0600

    clearing out typescript files

--- google/practiceTest.test.js
+++ google/practiceTest.test.js
@@ -1,10 +1,10 @@
 // Here in our .test.ts file we want to import some stuff from Selenium-Webdriver
 // We import Builder and Capabilities so we can build a driver that will allow us 
 // to interact with the browser
-const {Builder, Capabilities} = require('selenium-webdriver')
+const { Builder, Capabilities } = require('selenium-webdriver')
 
 // And we are also importing a function here that will do a search on Google
-const {search} = require('./src/search')
+const search = require('./src/search')
 
 // We ALWAYS need to require chromedriver. We don't need to save to a variable since we just need to bring in chromedriver once and not reference it again.
 require('chromedriver')
